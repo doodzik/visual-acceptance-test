fe2300e26a42019f481d9de4c5d242ab0e157975
Frederik Dudzik <frederik@dudzik.co>
2017-10-18 22:32:49 +0200

    add time module for last commit

--- roadmap.js
+++ roadmap.js
@@ -2,8 +2,20 @@
 
 const Browser   = require('./src/browser')
 const browser   = new Browser()
+
+const FileServer = require('./src/file-server')
 const staticDir = path.resolve('./to-build-dir')
 const server    = new FileServer({dir: staticDir})
+
+const Time      = require('./src/last-commit')
+const time      = new Time(__dirname)
+
+const fs = require('fs-extra')
+const path = require('path')
+
+function pathTo(dir) {
+  return path.resolve(__dirname, '.visual-acceptance-test', dir)
+}
 
 function build() {
   // TODO build
@@ -19,15 +31,23 @@ function screenshot({server, dir}) {
 }
 
 
-Promise.all([server.listen(), build()])
-.then(() => screenshot({server, dir: oldDir}))
-// TODO
+Promise.all([
+             server.listen(), 
+             build(), 
+             fs.remove(pathTo('HEAD'), 
+             fs.remove(pathTo('DIFF')
+            ])
+.then(() => screenshot({server, dir: pathTo('HEAD')}))
 .then(time.past)
 .then(build)
-.then(() => screenshot({server, dir: newDir}))
-// TODO
+.then(() => screenshot({server, dir: pathTo(time.pastCommit)}))
 .then(time.now)
-.then(() => image.diff({destination: diffDir, old: oldDir, new: newDir}))
+// TODO
+.then(() => image.diff({
+                        destination: pathTo('DIFF'),
+                        past: pathTo(time.pastCommit), 
+                        current: pathTo('HEAD'),
+                      }))
 .then(() => confirmation.browser({dir: diffDir}))
 // END TODO
 .then(server.destroy)
---
+++ src/last-commit.js
@@ -0,0 +1,33 @@
+const simpleGit = require('simple-git')
+
+class LastCommit {
+
+  constructor(workingDirPath) {
+    this.git = simpleGit(workingDirPath)
+    this.pastCommit = null
+  }
+
+  // make stash and change to last commit
+  past () {
+    return this.git.stash(['--all'])
+      .then(() => {
+         return this.git.log([
+                              '--branches -1',
+                              '--skip 1',
+                              '--pretty=format "%H"',
+                             ])
+      })
+      .then(commit => {
+        this.pastCommit = commit
+        this.git.checkout(commit)
+      })
+  }
+
+  // change to head and apply stash
+  now () {
+    return this.git.checkout('-')
+      .then(() => this.git.stash(['pop "stash@{0}"']))
+  }
+}
+
+module.exports = LastCommit
