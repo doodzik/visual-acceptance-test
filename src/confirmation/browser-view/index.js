29a0dc4c98c47ab8e5ac8ec5d0a23eeb43ecad5f
Frederik Dudzik <frederik@dudzik.co>
2017-12-05 19:05:53 +0100

    update

--- src/confirmation/browser-view/index.js
+++ src/confirmation/browser-view/index.js
@@ -1,13 +1,13 @@
 const {html, head, style, body, script} = require('javascript-html-tags')
 const cssRules = require('./css')
 const {testCases, actions}  = require('./content')
-const {toggleMaxWidth, toggleVisibility, reject, accept} = require('./js')
+const {toggleMaxWidth, toggleVisibility, accept, reject} = require('./js')
 
 function layout(testCases) {
 	return html(head(style(cssRules)),
 		body(testCases,
 			actions,
-			script(toggleMaxWidth, toggleVisibility, reject, accept)
+			script(toggleMaxWidth, toggleVisibility, accept, reject)
 		)
 	)
 }
